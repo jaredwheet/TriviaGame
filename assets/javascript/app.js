1adff7ad972be0b4180471ce4da22ce9b0f3a945
Jared Wheet <jaredwheet@gmail.com>
2018-10-30 18:02:56 -0700

    html css

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+//Opening page with start button
+
+// Timer Function including time converter to seconds?
+
+// Ability to get radio button values, compare to correct answers and increment correct/incorrect/unanswers
+
+//When Timer hits 0, show final results
+
+//Button to restart game
